40f03cb527b027b8e4468782e8e898f8c4211158
Piotr Kowalski <piecioshka@gmail.com>
2019-05-18 10:42:04 +0200

    Fix unit tests

--- src/app/components/app/app.component.spec.ts
+++ src/app/components/app/app.component.spec.ts
@@ -1,27 +1,27 @@
 import { TestBed, async } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
+import { ChannelListComponent } from '../channel-list/channel-list.component';
+import { ChannelListElementComponent } from '../channel-list-element/channel-list-element.component';
+
 describe('AppComponent', () => {
+  let fixture = null;
+  let component = null;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        ChannelListComponent,
+        ChannelListElementComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+
+  it('should create the component', async(() => {
+    expect(component).toBeTruthy();
   }));
 });
